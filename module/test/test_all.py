a878f2d5630a67bc33a7f265c8b11d49bcc274ab
clausia <ing.claudia@gmail.com>
2024-11-11 20:53:31 -0600

    add author to test_all.py

--- module/test/test_all.py
+++ module/test/test_all.py
@@ -1,3 +1,11 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Project: LILY-QML
+# Version: 2.0.0 LLY-DML
+# Author: Claudia Zendejas-Morales (@clausia)
+# Contact: info@lilyqml.de
+# Website: www.lilyqml.de
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 import os
 import tempfile
 import pytest
