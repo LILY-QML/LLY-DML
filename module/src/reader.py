7c213c6b681a9ce265715f92c6b65e0cfc80fb91
Supercabb <supercabb@gmail.com>
2024-11-14 17:11:04 +0000

    Added all.py functionality. Refactor Reader class to store train creation timestamp as a dictionary; update Circuit class to accept aer_backend parameter; enhance Optimizer class with error handling and logging; add unit tests for overall functionality.

--- module/src/reader.py
+++ module/src/reader.py
@@ -279,8 +279,7 @@ class Reader:
 
         current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        train_data = []
-        train_data.append({"creation:": current_datetime})
+        train_data = {"creation:": current_datetime}
 
         with open(train_file_path, 'w') as train_file:
             json.dump(train_data, train_file, indent=4)
